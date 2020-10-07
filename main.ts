67c9a6fbba06ee65a208ed5817bfa7d13ebe7c95
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-06 20:33:44 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+// Created by: Evan
+// Created on: Oct.2020
+// 
+// This program controls a servo motor with a potentiometer
+let Potentiometer = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    Potentiometer = pins.analogReadPin(AnalogPin.P1)
+    robotbit.Servo(robotbit.Servos.S1, pins.map(
+    Potentiometer,
+    0,
+    1023,
+    0,
+    180
+    ))
+    basic.pause(10)
 })
